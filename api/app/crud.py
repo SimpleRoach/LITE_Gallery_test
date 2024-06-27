334bf3d96d70d36e5acba0ee92c4bfbeae7e1b16
Ramil <tlsmscience@gmail.com>
2024-06-27 12:32:37 +0500

    Заполнил CRUD

---
+++ api/app/crud.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def get_project(db: Session, project_id: int):
+    return db.query(models.Project).filter(models.Project.id == project_id).first()
+
+def create_image(db: Session, image: schemas.ImageCreate):
+    db_image = models.Image(**image.dict())
+    db.add(db_image)
+    db.commit()
+    db.refresh(db_image)
+    return db_image
+
+def get_images(db: Session, project_id: int):
+    return db.query(models.Image).filter(models.Image.project_id == project_id).all()
